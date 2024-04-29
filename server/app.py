470f678a020c5daef30d564b4e8ed0319db232e1
estherwanjiku007 <estherwanjiqu@gmail.com>
2024-04-29 10:08:05 +0300

    Made the correct changes

--- server/app.py
+++ server/app.py
@@ -1,12 +1,12 @@
 # server/app.py
 #!/usr/bin/env python3
-
 from flask import Flask
 from flask_migrate import Migrate
 
 from models import db
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
--- server/models.py
+++ server/models.py
@@ -7,17 +7,28 @@ from sqlalchemy.ext.associationproxy import association_proxy
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
-
 db = SQLAlchemy(metadata=metadata)
-
-
+employee_meetings = db.Table(
+    'employees_meetings',
+    metadata,
+    db.Column('employee_id', db.Integer, db.ForeignKey(
+        'employees.id'), primary_key=True),
+    db.Column('meeting_id', db.Integer, db.ForeignKey(
+        'meetings.id'), primary_key=True)
+)
 class Employee(db.Model):
     __tablename__ = 'employees'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
+    meetings=db.relationship("Meeting",secondary=employee_meetings,back_populates="employees")
+    assingnments=db.relationship("Assingment",back_populates="employees",cascade="all ,delete-orphan")
+    projects=association_proxy("assingnments","project",creator=lambda project_obj:Assingnment(project=project_obj))
+#Assingnment
 
+    # assignments=db.relationship("Assingnment",back_populates="assignment",cascade="all, delete-orphan")
+    # projects=association_proxy("assignments","project",creator=lambda project_obj:Assingnment(project=project_obj))
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.hire_date}>'
 
@@ -29,7 +40,7 @@ class Meeting(db.Model):
     topic = db.Column(db.String)
     scheduled_time = db.Column(db.DateTime)
     location = db.Column(db.String)
-
+    employees=db.relationship("Employee" ,secondary=employee_meetings,back_populates="meetings")
     def __repr__(self):
         return f'<Meeting {self.id}, {self.topic}, {self.scheduled_time}, {self.location}>'
 
@@ -40,6 +51,39 @@ class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     budget = db.Column(db.Integer)
+    assingnments=db.relationship("Assingnment",back_populates="projects",cascade="all ,delete-orphan")
+    employees=association_proxy("assingnments","employee",creator=lambda employee_obj:Assingnment(employee=employee_obj))
 
+    # assignments=db.relationship("Assingnment",back_populates="project")
+    # employees=association_proxy("assignments","employees",creator=lambda employee_obj: Assingnment(employees=employee_obj))
     def __repr__(self):
         return f'<Review {self.id}, {self.title}, {self.budget}>'
+
+# class Assingnment(db.Model):
+#     __tablename__="assignments"
+
+#     id=db.Column(db.Integer,primary_key=True)
+#     role=db.Column(db.String)
+#     start_date=db.Column(db.DateTime)
+#     end_date=db.Column(db.DateTime)
+#     employee_id=db.Column(db.Integer,db.ForeignKey("employees.id"))
+#     project_id=db.Column(db.Integer,db.ForeignKey("projects.id"))
+#     employee=db.relationship("Employee",back_populates="assigmnents")
+#     project=db.relationship("Project",back_populates="assigmments")
+
+#     def __repr__(self):
+#         return f"Assignment {self.id} {self.role} {self.start_date} {self.end_date} {self.employee.name} {self.project.title}"
+class Assingnment(db.Model):
+    __tablename__="assignments"
+
+    id=db.Column(db.Integer,primary_key=True)
+    role=db.Column(db.String)
+    start_date=db.Column(db.DateTime)
+    end_date=db.Column(db.DateTime)
+
+    employee_id=db.Column(db.Integer,db.ForeignKey("employees.id"))
+    project_id=db.Column(db.Integer,db.ForeignKey("projects.id"))
+    employees=db.relationship("Employee",back_populates="assignments")
+    projects=db.relationship("Project",back_populates="assignments")
+    
+
